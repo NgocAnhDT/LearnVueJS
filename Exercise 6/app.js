3633c574f406d25eefd2aae0d899fd58bd800271
NgocAnhDT <dtngocanh158@gmail.com>
2022-07-13 13:05:33 +0700

    mini prọect

---
+++ Exercise 6/app.js
@@ -0,0 +1,32 @@
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        };
+    },
+    methods: {
+        attackMonster() {
+            if (this.monsterHealth < 6) {
+                this.monsterHealth = 0;
+            } else {
+                const attackValue = getRandomValue(5, 12);
+                this.monsterHealth -= attackValue;
+            }
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            if (this.playerHealth < 9) {
+                this.playerHealth = 0;
+            } else {
+                const attackValue = getRandomValue(8, 15);
+                this.playerHealth -= attackValue;
+            }
+        }
+    },
+});
+
+app.mount('#game');
